24307ca7898c7846bec6318db6e5212a4a97db1d
Van Thong Nguyen <tom@gridscale.io>
2021-04-21 17:29:27 +0200

    add main.go

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	fileUtils "github.com/nvthongswansea/xtreme/internal/file-utils"
+	restful "github.com/nvthongswansea/xtreme/internal/fman/delivery/restful"
+	_fmanRepo "github.com/nvthongswansea/xtreme/internal/fman/repo"
+	_fmanUC "github.com/nvthongswansea/xtreme/internal/fman/usecase"
+	uuidUtils "github.com/nvthongswansea/xtreme/internal/uuid-utils"
+)
+
+var basePath string
+
+func init() {
+	// Get basePath from cmd args
+	flag.StringVar(&basePath, "base_path", "", "Base path that stores all files")
+	flag.Parse()
+	if basePath == "" {
+		fmt.Println("base_path arg is missing!")
+		os.Exit(1)
+	}
+}
 
 func main() {
-	fmt.Println("Hello world")
+	sqliteRepo := _fmanRepo.NewFManSQLiteRepo()
+	uuidGenerator := &uuidUtils.GoogleUUIDGenerator{}
+	fileSaver := fileUtils.CreateNewLocalFileOperator(basePath)
+	fmanUC := _fmanUC.NewFManLocalUsecase(sqliteRepo, uuidGenerator, fileSaver)
+	//Start web service
+	e := echo.New()
+	restful.InitFmanHandler(e, fmanUC)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Logger.Fatal(e.Start(":4000"))
 }
