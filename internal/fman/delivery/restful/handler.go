9662430c88262f22e54ea84d22aab12e53930563
Van Thong Nguyen <tom@gridscale.io>
2021-04-21 17:28:37 +0200

    add restful handler for fman upload file

---
+++ internal/fman/delivery/restful/handler.go
@@ -0,0 +1,46 @@
+package restful
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/nvthongswansea/xtreme/internal/fman"
+	"github.com/nvthongswansea/xtreme/internal/models"
+)
+
+// ResponseError represents http response error in JSON format
+type Response struct {
+	Message string `json:"message"`
+}
+
+// FmanHandler represents the http handler for file manage
+type FmanHandler struct {
+	FmanUsecase fman.FmanUsecase
+}
+
+// InitFmanHandler initialize file manager endpoints
+func InitFmanHandler(e *echo.Echo, uc fman.FmanUsecase) {
+	handler := &FmanHandler{FmanUsecase: uc}
+	g := e.Group("/fman")
+	g.POST("/file", handler.UploadNewFile)
+}
+
+func (h *FmanHandler) UploadNewFile(c echo.Context) error {
+	// Get file from form
+	file, err := c.FormFile("file")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Save file
+	err = h.FmanUsecase.UploadFile(models.File{}, src)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, Response{Message: "Uploaded file successfully"})
+}
