234d001a32ddb8497528851675efdd3b2022ae51
Van Thong Nguyen <tom@gridscale.io>
2021-05-13 18:16:55 +0200

    reconstruct LocalFManRepository

--- internal/database/database.go
+++ internal/database/database.go
@@ -8,30 +8,12 @@ import (
 // LocalFManRepository holds all operations on the db,
 // which are used by local storage's file manager.
 type LocalFManRepository interface {
-	DBEntityMetadataInserter
-	DBEntityGetter
-	DBEntitySearcher
-	DBEntityMetadataUpdater
-	DBEntityRemover
-	DBEntityValidator
-}
-
-// DBEntityInserter holds inserting operations on db.
-type DBEntityInserter interface {
-	DBEntityMetadataInserter
-}
-
-// DBEntityMetadataInserter holds metadata inserting operations on db.
-type DBEntityMetadataInserter interface {
 	// InsertFileMetadata inserts a new file metadata to db.
 	InsertFileMetadata(ctx context.Context, newFile models.FileMetadata) error
 
 	// InsertDirectoryMetadata inserts a directory/folder metadata to db.
 	InsertDirectoryMetadata(ctx context.Context, newDir models.DirectoryMetadata) error
-}
 
-// DBEntityGetter holds retrieval operations on db.
-type DBEntityGetter interface {
 	// GetFile gets a file record from the db with a given UUID.
 	GetFile(ctx context.Context, fileUUID string) (models.File, error)
 
@@ -48,11 +30,6 @@ type DBEntityGetter interface {
 	// of a specific directory.
 	GetDirectChildDirUUIDList(ctx context.Context, dirUUID string) ([]string, error)
 
-	DBEntityMetadataGetter
-}
-
-// DBEntityMetadataGetter holds metadata retrieval operations on db.
-type DBEntityMetadataGetter interface {
 	// GetFileMetadata gets file metadata from the db with a given UUID.
 	GetFileMetadata(ctx context.Context, fileUUID string) (models.FileMetadata, error)
 
@@ -67,24 +44,13 @@ type DBEntityMetadataGetter interface {
 
 	// GetDirMetadata gets directory/folder metadata from the db with a given UUID.
 	GetDirMetadata(ctx context.Context, dirUUID string) (models.DirectoryMetadata, error)
-}
 
-// DBEntityUpdater holds updating operations on db.
-type DBEntityUpdater interface {
-	DBEntityMetadataUpdater
-}
-
-// DBEntityMetadataUpdater holds metadata updating operations on db
-type DBEntityMetadataUpdater interface {
 	// UpdateFileMetadata updates a file metadata in the db.
 	UpdateFileMetadata(ctx context.Context, newMetadata models.FileMetadata) error
 
 	// UpdateDirMetadata updates a directory/folder metadata in the db.
 	UpdateDirMetadata(ctx context.Context, newMetadata models.DirectoryMetadata) error
-}
 
-// DBEntityRemover holds removing operations on db.
-type DBEntityRemover interface {
 	// SoftRemoveFile flags a file record as deleted file.
 	// e.g. set `is_deleted` field to true.
 	SoftRemoveFile(ctx context.Context, fileUUID string) error
@@ -98,16 +64,21 @@ type DBEntityRemover interface {
 
 	// HardRemoveDir removes a directory/folder record completely from the db.
 	HardRemoveDir(ctx context.Context, dirUUID string, fileRmCallback func(string) error) error
-}
 
-// DBEntityValidator holds validating operations on db.
-type DBEntityValidator interface {
 	// IsNameExist checks if a specific file/dir's name exists in a specific directory.
 	IsNameExist(ctx context.Context, name, parentDirUUID string) (bool, error)
-}
 
-// DBEntitySearcher holds searching operations on db.
-type DBEntitySearcher interface {
 	// SearchByName searches all files/directories based on given filename within a specific path.
 	SearchByName(ctx context.Context, userUUID, filename, parentDirUUID string) ([]models.File, []models.Directory, error)
+}
+
+
+type UserRoleByEntityGetter interface {
+	// GetUserRoleByFile returns role of a user upon the given file.
+	// Current accepted roles: "owner", "editor", "viewer".
+	GetUserRoleByFile(ctx context.Context, userUUID, fileUUID string) (string, error)
+
+	// GetUserRoleByDirectory returns role of a user upon the given directory.
+	// Current accepted roles: "owner", "editor", "viewer".
+	GetUserRoleByDirectory(ctx context.Context, userUUID, dirUUID string) (string, error)
 }
